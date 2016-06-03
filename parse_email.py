e72a9d0466b9c9cb5a1a7eacd5fd7af439da621d
Frank Cheng <ufo22940268@gmail.com>
2016-06-03 13:24:09 +0800

    Test whether 'test' string can be found in title and body of email.

--- parse_email.py
+++ parse_email.py
@@ -1,5 +1,6 @@
 from gmail import Gmail
 
+
 # Print body of all emails.
 #
 # Usage
@@ -8,9 +9,15 @@ from gmail import Gmail
 # - cd gmail && python setup.py install
 # - cd .. && python parse_email.py
 
-g = Gmail()
-g.login('aabbccni@gmail.com', 'a12345678')
-for mail in g.inbox().mail():
-    mail.fetch()
-    print mail.body
-g.logout()
+def find_test(g):
+    for mail in g.inbox().mail(unread=True):
+        mail.fetch()
+        if 'test' in mail.subject and 'test' in mail.body:
+            return True
+    return False
+
+if __name__ == '__main__':
+    g = Gmail()
+    g.login('aabbccni@gmail.com', 'a12345678')
+    print 'Test can be found in body and title' if find_test(g) else 'Test not found in body and title'
+    g.logout()
